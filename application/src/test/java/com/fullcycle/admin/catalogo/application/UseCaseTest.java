a32efc6a853f18823a674605af3fe2edb32b3380
ednaldo <ednaldo.contato1@gmail.com>
2024-04-15 16:38:30 -0300

    Configurando os submódulos do projeto base

---
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/UseCaseTest.java
@@ -0,0 +1,10 @@
+package com.fullcycle.admin.catalogo.application;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+public class UseCaseTest {
+    @Test
+    public void testCreateUseCase(){
+        Assertions.assertNotNull(new UseCase());
+        Assertions.assertNotNull(new UseCase().execute());
+    }
+}
